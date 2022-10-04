90e193f8fd68edc3b2c530695410b3c8baa9de6b
Azreil-OFD <fantom2413@gmail.com>
2022-10-04 20:47:24 +0300

    add static

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 
 
+
+
+
+// обработка статики
+app.use(express.static(path.resolve(__dirname , 'client')))
+app.get(
+    '*',
+    (req , res) => {
+        res.sendFile(path.resolve(__dirname , 'client' , 'index.html'))
+    }
+)
 
 
 
